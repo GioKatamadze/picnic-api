30d08bb0a217273ff49bf0bbb47a9fa33d5ec56f
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-21 01:04:30 +0400

    create api for answering get requests from frontend, (test)

--- src/server.js
+++ src/server.js
@@ -6,7 +6,13 @@ import fs from "fs";
 const app = express();
 const results = [];
 
-fs.createReadStream("./data/Whatsgoodly-10.csv")
-  .pipe(csvParser)
-  .on("data", (data) => results.push(data))
-  .on("end", _);
+app.use(cors());
+app.get("/", (_, res) => {
+  fs.createReadStream("./data/Whatsgoodly-10.csv")
+    .pipe(csvParser)
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      console.log(results);
+    });
+});
+app.listen(process.env.PORT || 5000);
