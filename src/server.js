70cd0a6c7f5024856f3ed90410b1b16826e04d47
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-21 00:53:12 +0400

    create csv parser function to pass data inside the results varieable for frontend

--- src/server.js
+++ src/server.js
@@ -4,3 +4,9 @@ import csvParser from "csv-parser";
 import fs from "fs";
 
 const app = express();
+const results = [];
+
+fs.createReadStream("./data/Whatsgoodly-10.csv")
+  .pipe(csvParser)
+  .on("data", (data) => results.push(data))
+  .on("end", _);
