3d9884f168dedf36042d6130e335cf1f52b475e2
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-21 15:04:01 +0400

    fix data location path error, clean final code

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
-import { Express } from "express";
+import express from "express";
 import cors from "cors";
 import csvParser from "csv-parser";
 import fs from "fs";
 
 const app = express();
-const results = [];
 
-app.use(cors());
-app.get("/", (_, res) => {
-  fs.createReadStream("./data/Whatsgoodly-10.csv")
-    .pipe(csvParser)
+app.get("/", cors(), (_, res) => {
+  const results = [];
+  fs.createReadStream("./src/data/WhatsgoodlyData-10.csv")
+    .pipe(csvParser())
     .on("data", (data) => results.push(data))
     .on("end", () => {
       res.status(200).json(results);
     });
 });
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
